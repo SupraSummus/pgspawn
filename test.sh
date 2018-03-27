4ce876a3ba01abf540ee6ddb19aa444b0ae46b66
Jan Rydzewski <flegmer@gmail.com>
2018-03-27 19:49:57 +0200

    tests

--- test.sh
+++ test.sh
@@ -2,10 +2,14 @@
 set -e
 
 function assert {
-	diff /proc/self/fd/0 <(echo -en $1)
+	diff /proc/self/fd/0 <(echo -en "$1")
 }
 
 echo abcd | pgspawn examples/id.yml | assert 'abcd\n'
+echo abcd | pgspawn examples/id_explicite.yml | assert 'abcd\n'
+
 pgspawn examples/join.yml | sort | assert 'one\nthree\ntwo\n'
 pgspawn examples/join_explicite.yml | sort | assert 'one\nthree\ntwo\n'
-#pgspawn examples/join_pipe.yml | sort | assert 'one\nthree\ntwo\n'
+pgspawn examples/join_pipe.yml | sort | assert 'one\nthree\ntwo\n'
+
+echo -en 'a\nb\nc\n' | pgspawn examples/split_pipe.yml | sort | assert 'a\nb\n'
